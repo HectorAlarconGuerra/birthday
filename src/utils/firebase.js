62e9f8164f29212e1ec5e0021326bf2245a5183f
hectoralarcon <hectoralarconguerra@gmail.com>
2020-11-22 17:18:51 -0500

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCT2-eZn6hFtyMpaFL_ADLDw0rK145TEDY',
+  authDomain: 'birthday-4840b.firebaseapp.com',
+  databaseURL: 'https://birthday-4840b.firebaseio.com',
+  projectId: 'birthday-4840br',
+  storageBucket: 'birthday-4840b.appspot.com',
+  messagingSenderId: '57367422131',
+  appId: '1:57367422131:web:1620bcd403a6d98f81bcc6',
+};
+
+export default firebase.initializeApp(firebaseConfig);
